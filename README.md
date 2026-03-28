e8e503a78971bdb3d1328de63cd6102da58b05d7
Anjali <reddyanjali2016@gmail.com>
2026-03-28 18:55:55 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Spring Boot + MySQL CRUD Project
+
+## Setup
+1. Create MySQL DB:
+   CREATE DATABASE testdb;
+
+2. Update username/password in application.properties
+
+3. Run project:
+   mvn spring-boot:run
+
+## APIs
+
+POST   /users
+GET    /users
+GET    /users/{id}
+DELETE /users/{id}
---
+++ src/main/java/com/example/crud/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.crud.controller;
+
+import com.example.crud.entity.User;
+import com.example.crud.service.UserService;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @Autowired
+	private Environment environment;
+    
+    @GetMapping("/g")
+    public String get() {
+    	String  name=environment.getProperty("app.name");
+    	return name;
+    }
+
+    @GetMapping("/get")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
